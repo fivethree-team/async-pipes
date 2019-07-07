cad81112ad573344732743454f4cfea0903780ab
Marc Stammerjohann <marcstammerjohann@web.de>
2019-07-07 18:39:31 +0200

    chore(example): update example

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { delay, flatMap } from 'rxjs/operators';
 
 @Component({
@@ -8,14 +8,20 @@ import { delay, flatMap } from 'rxjs/operators';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  numbers$: Observable<string>;
+  numbers$: Observable<number[]>;
+  numbersEmpty$: Observable<number[]>;
+  numbersError$: Observable<number[]>;
 
   constructor() {}
 
   ionViewWillEnter() {
-    this.numbers$ = of('').pipe(
-      delay(5000)
-      // flatMap(() => throwError('ups'))
+    this.numbers$ = of([1, 2, 3, 4, 5]).pipe(delay(5000));
+
+    this.numbersEmpty$ = of([]).pipe(delay(5000));
+
+    this.numbersError$ = of([1, 2, 3, 4, 5]).pipe(
+      delay(5000),
+      flatMap(() => throwError('ups'))
     );
   }
 }
